27a044f21c7369a485de531b1131cd193f9474c8
PRIFTI <dionis.prifti.sp@lhind.dlh.de>
2019-05-01 11:41:10 +0200

    Finished initial Gui, the images, control elements and the grid.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,67 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
+        VBox mainLayout = new VBox();
+        HBox controlLayout = new HBox();
+
+        ObservableList<String> options = 
+            FXCollections.observableArrayList(
+                "Easy",
+                "Medium",
+                "Hard"
+            );
+        ComboBox<String> gameDifficultyCombo = new ComboBox<String>(options);
         
-        Label l = new Label("Building OO Minesweeper with JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Button newGameBtn = new Button("New Game");
+
+        Image smilingImage = new Image(getClass().getResourceAsStream("images/smiling.png"));
+        ImageView smilingImageView = new ImageView(smilingImage);
+
+        Image flagImage = new Image(getClass().getResourceAsStream("images/flag.png"));
+        ImageView flagImageView = new ImageView(flagImage);
+        Label flagsNumberLabel = new Label("04");
+
+        Image mineImage = new Image(getClass().getResourceAsStream("images/mine.png"));
+        ImageView mineImageView = new ImageView(mineImage);
+        Label totalMinesLabel = new Label("99");
         
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        controlLayout.getChildren().addAll(gameDifficultyCombo, newGameBtn, 
+            smilingImageView, flagImageView, flagsNumberLabel, mineImageView, totalMinesLabel);
+        
+        //Building a 9x9 grid
+        TilePane tile = new TilePane();
+        tile.setPadding(new Insets(5, 0, 5, 0));
+        tile.setPrefColumns(16);
+        tile.setPrefColumns(16);
+
+        ToggleButton cells[][] = new ToggleButton[16][16];
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                cells[i][j] = new ToggleButton("  ");
+                tile.getChildren().add(cells[i][j]);
+            }
+        }
+
+        mainLayout.getChildren().addAll(controlLayout, tile);
+        
+        Scene scene = new Scene(mainLayout);
         stage.setScene(scene);
         stage.show();
     }

bdfccd7bf091e3fa29b035d524e579f25214c784
PRIFTI <dionis.prifti.sp@lhind.dlh.de>
2019-05-05 13:47:28 +0200

    Part-8: Added the Info Class for the message dialogs.

---
+++ src/main/java/util/Info.java
@@ -0,0 +1,47 @@
+package util;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.scene.control.ButtonType;
+
+public class Info {
+
+    public Info() {
+    }
+
+    public static final Logger LOGGER = Logger.getLogger(Info.class.getName());
+
+    public static Boolean showConfirmationDialog(String headerText, String contentText) {
+        LOGGER.log(Level.INFO, headerText + ": " + contentText);
+
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("images/mine.png"));
+
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+
+	ButtonType okButton = ButtonType.YES;
+	ButtonType noButton = ButtonType.NO;
+	alert.getButtonTypes().setAll(okButton, noButton);
+	alert.showAndWait();
+
+	if (alert.getResult() == ButtonType.YES)
+	    return true;
+
+	return false;
+    }
+
+    public static Boolean showConfirmationDialogStartNewGame() {
+        String header = "Start a new game";
+	String content = "Are you sure you want to start a new game? The current game will be lost!";
+
+	return showConfirmationDialog(header, content);
+    }
+
+}

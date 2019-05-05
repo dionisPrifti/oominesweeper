fce27831d9b67759c1ef80ddf82cde88ae215c2a
PRIFTI <dionis.prifti.sp@lhind.dlh.de>
2019-05-06 00:31:29 +0200

    Part-9: Add the getNeighbors method.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,10 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import util.BoardUtil;
 
 public class Board {
 
-    int width;
-
-    int height;
+    int totalRows; //height
+    
+    int totalCols; //width
     
     Cell[][] cells;
     
+    /**
+     * Get a list of all the neighbor cells 
+     * 
+     * @param row
+     * @param col
+     * @return
+     */
+    List<Cell> getNeighbours(int row, int col) {
+        List<Cell> neighbours = new ArrayList<>();
+        
+        for (int rowOffset : BoardUtil.OFFSETS) {
+            for (int colOffset : BoardUtil.OFFSETS) {
+                //Do not include the cell itself
+                if (! (rowOffset == 0 && colOffset == 0)) {
+                    
+                    //Add as a neighbor only if it is a valid cell 
+                    //(within the range of the board)
+                    if (BoardUtil.isValid(row + rowOffset, col + colOffset, totalRows, totalCols)) {
+                        neighbours.add(cells[row + rowOffset][col + colOffset]);
+                    }
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
 }

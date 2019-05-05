b6edcba0d7eb532f7f010f56aa879014b8118eb4
PRIFTI <dionis.prifti.sp@lhind.dlh.de>
2019-05-06 01:13:01 +0200

    Part-10: Add row, col, revealed in Cell. Introduced Encapsulation.

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,10 +1,33 @@
 
 public class Cell {
 
+    private int row;
+    
+    private int col;
+    
     /**
      * 0 for a blank cell
      * -1 for a mine
      * 1-8 for a digit representing the surrounding mines
      */
-    int value;
+    private int value;
+    
+    private boolean revealed;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+    
 }
